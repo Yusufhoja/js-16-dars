c9a0b7b8eb21a57138ee8c5c1f69b931487cd281
Yusufhoja <usufhozah@gmail.com>
2024-04-05 14:43:21 +0500

    js 16-dars

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+var isim = prompt("Ismingizni kiriting");
+var index = document.createElement("div");
+index.textContent =  isim;
+document.body.appendChild(index);
+
+// // Foydalanuvchidan ismini so'raymiz
+// var ism = prompt("Ismingizni kiriting:");
+
+// // Yangi div elementini yaratamiz
+// var yangiDiv = document.createElement("div");
+
+// // Divga ismni qo'shamiz
+// yangiDiv.textContent = ism;
+
+// // Divga ushbu stili qo'shamiz: 128 piksel hajmi
+// yangiDiv.style.width = "128px";
+// yangiDiv.style.height = "128px";
+// yangiDiv.style.border = "1px solid black";
+
+// // Divni bodyga qo'shamiz
+// document.body.appendChild(yangiDiv);
